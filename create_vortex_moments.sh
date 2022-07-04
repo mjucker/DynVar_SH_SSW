e23ef857f9e80d9064566746fc988131d0d1ef18
Martin Jucker <coding@martinjucker.com>
2022-07-04 12:11:54 +1000

    renaming bash script to create vortex moments

--- create_vortex_moments.sh
+++ create_vortex_moments.sh
@@ -22,7 +22,9 @@ do
 	    edge=$(echo "${mean_dict[$level]}$delta_edge" |bc -l)
 	    echo "$file; ${level}hPa, ${edge}km"
             ncap2 -s 'z=z/9.81' $file -O tmp.nc
-	    outFile=ERA5_vxmoms_${level}hPa_${edge}km.nc
+	    pre=${file%.z.nc}
+	    year=${pre#*.}
+	    outFile=ERA5_vxmoms_${year}_${level}hPa_${edge}km.nc
 	    python $repdir/DynVar_SH_SSW/compute_vortex_moments.py -z tmp.nc -l $level -e $edge -o $outFile
 	done
     done
